5d48aa3843dff01de129d56090517547c112a3fd
Mingfeng Li <lmf950122@gmail.com>
2021-07-20 21:29:02 +0000

    Add comments for understanding

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,16 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # VIEW
+    #render :index / behind the theme
   end
 
   def show
+    #instance variable @prodect is automaticlly transfered between controller into the view
     @product = Product.find params[:id]
+    #render :show implecit
   end
 
 end
