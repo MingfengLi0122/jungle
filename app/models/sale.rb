5b5a07123a0b337b0f535cd9a51e2145e1a0e698
Mingfeng Li <lmf950122@gmail.com>
2021-07-23 03:14:43 +0000

    Create model for sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,20 @@
 class Sale < ActiveRecord::Base
+  # socpe :active, -> {  where("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current) }
+  # self.active class method AR scope
+  def self.active
+    where("sales.starts_on < ? AND sales.ends_on >= ?", Date.current, Date.current)
+  end
+
+  def finished?
+    # equals to self.ends_on
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
 end
